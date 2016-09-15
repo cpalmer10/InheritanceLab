0ea03c4a95d2bad26aae605a14596ab96a99a272
cpalmer10 <cpalmer10@my.wctc.edu>
2016-09-15 14:36:05 -0500

    added imaginary/realworld abstractions

---
+++ src/imaginary/object1/Zeus.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imaginary.object1;
+
+/**
+ *
+ * @author Chris
+ */
+public class Zeus extends MythicalFigure{
+    private String callThunder;
+    private String transform;
+
+    public String getCallThunder() {
+        return callThunder;
+    }
+
+    public void setCallThunder(String callThunder) {
+        this.callThunder = callThunder;
+    }
+
+    public String getTransform() {
+        return transform;
+    }
+
+    public void setTransform(String transform) {
+        this.transform = transform;
+    }
+    
+    
+    @Override
+    public void sayPrayer() {
+        System.out.println("Zeus... God of thunder...");
+    }
+}
